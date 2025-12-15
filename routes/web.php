074eddbac08358220aad0a6650c23c450a76653e
Osmar Alejandro Liera Gomez <osmar2001lg@gmail.com>
2025-12-15 10:53:38 -0700

    sin stripe

--- routes/web.php
+++ routes/web.php
@@ -6,10 +6,3 @@ use App\Http\Controllers\StripeController;
 Route::get('/', function () {
     return view('welcome');
 });
-
-Route::get('/checkout', function () {
-    return view('checkout');
-});
-
-
-Route::post('/create-payment-intent', [StripeController::class, 'createPaymentIntent']);
